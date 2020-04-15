8d4bbf69543840f6e82246bc7d5bb61237cb76e6
Justin Albrecht <jalbrec1@umd.edu>
2020-04-15 16:41:27 -0400

    updated variable names

--- Code/LKtracker.py
+++ Code/LKtracker.py
@@ -3,7 +3,7 @@ import numpy
 
 def affineLKtracker(I,T,rect,p_prev):
     # I is a grayscale image of the current frame
-    # T is the template image 
+    # T is the template image in grayscale
     # rect is the bounding box tht marks the template region in tmp 
         # tuple of corners c1,c2 with their associated x and y values
         # ((c1x,c1y),(c2x,c2y))
@@ -15,15 +15,15 @@ def affineLKtracker(I,T,rect,p_prev):
 
         # Step 1a: Define an affine warp W using p_prev
 
-        # Step 1b: Warp img using W to get I_w
+        # Step 1b: Warp I using W to get I_w
 
-        # Step 2: Compute the error image tmp - I_w (err)
+        # Step 2: Compute the error image T - I_w (err)
 
         # Step 3a: Compute the gradients of I (I_x, I_y)
 
         # Step 3b: Warp I_x and I_y using W
 
-        # Step 4: Evaluate the Jacobin dW/dp at (x;p) (J)
+        # Step 4: Evaluate the Jacobian dW/dp at (x;p) (J)
 
         # Step 5: Compute the steepest descent images delI*J (SDI)
 
